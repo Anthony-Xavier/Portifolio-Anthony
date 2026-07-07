e16f57149a01d8d6752da11b944cbfb199056c9a
Anthony Xavier <anthonyltx12@gmail.com>
2026-07-07 13:07:01 -0300

    cores e fundo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
-import "./App.scss";
+import "./styles/main.scss"; // Dica: se o main.scss já importa tudo, você pode remover o App.scss daqui se não for usar
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    // A div agora envelopa o conteúdo e cobre a tela toda
+    <div className="blueprint-background"></div>
+  );
 }
 
 export default App;
